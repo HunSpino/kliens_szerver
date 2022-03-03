d48f27200a5ff19d0676b62a10a1ed8dbe98fc40
HunSpino <tamasleventelaszlo99@gmail.com>
2022-03-03 11:09:58 +0100

    haladás

--- src/feladat04/Kliens.java
+++ src/feladat04/Kliens.java
@@ -1,7 +1,38 @@
 package feladat04;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Kliens {
     public static void main(String[]args){
+        try{
+            Socket kapcsolat = new Socket("localhost",8080);
+            DataInputStream servertol = new DataInputStream(
+                    kapcsolat.getInputStream());
+            DataOutputStream servernek = new DataOutputStream(
+                    kapcsolat.getOutputStream());
+            int menu;
+            Scanner sc = new Scanner(System.in);
+            do{
+                System.out.println("Válasszon a menüpontok közül: ");
+                System.out.println("1. Feladat");
+                System.out.println("2. Feladat");
+                System.out.println("3. Feladat");
+                System.out.println("4. Feladat");
+                System.out.println("5. Feladat");
+                System.out.println("6. Kilépés");
+                menu = sc.nextInt();
+                servernek.writeInt(menu);
+                servernek.flush();
+                System.out.println(servertol.readUTF());
+            }while (menu !=6);
 
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
--- src/feladat04/Szerver.java
+++ src/feladat04/Szerver.java
@@ -1,7 +1,32 @@
 package feladat04;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Szerver {
     public static void main(String[] args){
+
         UgyfelKiszolgalo u = new UgyfelKiszolgalo();
+        ExecutorService exe = Executors.newCachedThreadPool();
+        System.out.println("A szerver indul....");
+
+        try{
+            ServerSocket socket = new ServerSocket(8080);
+            while(true){
+                Socket kapcsolat = socket.accept();
+                InetAddress ugyfel = kapcsolat.getInetAddress();
+
+                feladat03.UgyfelKiszolgalo uk = new feladat03.UgyfelKiszolgalo(kapcsolat);
+                exe.submit(uk);
+            }
+
+
+        }catch (IOException ex){
+            System.out.println(ex);
+        }
     }
 }
